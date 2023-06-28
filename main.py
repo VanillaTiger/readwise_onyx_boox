630c21ca819b61af735fdf35e06bc24cd43c2fda
Adam Szummer <adam.szummer@digica.com>
2023-06-28 23:04:13 +0200

    flake8 fixes and zero to one

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import argparse
 from thought_processing.thought import WiseThought
 from thought_processing.files_processing import load_new_book_annotation, prepare_file_and_headers, save_thought_to_csv
 
+
 def main(filepath, output_path, author, title):
     annotation_text = load_new_book_annotation(filepath)
     prepare_file_and_headers(output_path)
 
     data_splited = annotation_text.split('-------------------')
     for item in data_splited:
-        thought = WiseThought(item,author, title)
+        thought = WiseThought(item, author, title)
         try:
             row = thought.parse_thought()
             save_thought_to_csv(row, output_path)
         except(AttributeError):
-            if item != '':
-                print(f"Error in thought: {item}")
+            if item == "\n":
+                print("Finished")
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -24,6 +26,7 @@ def parse_args():
     parser.add_argument("--output_path")
     args = parser.parse_args()
     return args.author, args.title, args.filepath, args.output_path
+
 
 if __name__ == "__main__":
     author, title, filepath, output_path = parse_args()
